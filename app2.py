908253d71bafa58a45fd523b2bd301ca73f326d9
richurachel2408-web <richurachel2408@gmail.com>
2026-07-03 14:13:04 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,20 +11,7 @@ import yt_dlp
 
 # --- 1. INITIALIZE STATE ---
 if 'audio_path' not in st.session_state:
-    st.session_state['audio_path']import streamlit as st
-import os
-import glob
-
-# Handling MoviePy version differences (v1.x vs v2.x)
-try:
-    from moviepy.editor import ImageClip, concatenate_videoclips, AudioFileClip
-except ImportError:
-    from moviepy import ImageClip, concatenate_videoclips, AudioFileClip
-import yt_dlp
-
-# --- 1. INITIALIZE STATE ---
-if 'audio_path' not in st.session_state:
-    st.session_state['audio_path'] = None
+    st.session_state['audio_path'] = None 
 if 'yt_error' in st.session_state:
     pass # Keep it for display logic
 
